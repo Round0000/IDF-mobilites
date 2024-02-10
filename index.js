6a58d4ba4e9c000262504ef1e217d768f3115314
unknown <billybinche@gmail.com>
2024-02-10 14:58:00 +0100

    safe value accessors

--- index.js
+++ index.js
@@ -24,10 +24,20 @@ function getChateletRerB(res) {
       json.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit.forEach(
         (item) => {
           if (
-            item.MonitoredVehicleJourney.LineRef.value !== "STIF:Line::C01743:"
-          ) return;
-          if (!item.MonitoredVehicleJourney.DirectionName[0].value.includes('AEROPORT')) return;
-          if (item.MonitoredVehicleJourney.MonitoredCall.DepartureStatus === "cancelled") return;
+            item.MonitoredVehicleJourney.LineRef?.value !== "STIF:Line::C01743:"
+          )
+            return;
+          if (
+            !item.MonitoredVehicleJourney.DirectionName[0]?.value.includes(
+              "AEROPORT"
+            )
+          )
+            return;
+          if (
+            item.MonitoredVehicleJourney.MonitoredCall.DepartureStatus ===
+            "cancelled"
+          )
+            return;
 
           let time = new Date(
             item.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime
@@ -35,29 +45,54 @@ function getChateletRerB(res) {
 
           if (new Date() > time) return;
 
-          const timeFormatter = Intl.DateTimeFormat('fr-FR', { timeZone: "Europe/Paris", timeStyle: 'medium' });
+          const timeFormatter = Intl.DateTimeFormat("fr-FR", {
+            timeZone: "Europe/Paris",
+            timeStyle: "medium",
+          });
           time = timeFormatter.format(time);
 
-          const destination = item.MonitoredVehicleJourney.MonitoredCall.DestinationDisplay[0].value.slice(0, 26) + ' ...';
+          const destination =
+            item.MonitoredVehicleJourney.MonitoredCall.DestinationDisplay[0]?.value.slice(
+              0,
+              26
+            ) + " ...";
+
+            console.log(destination)
 
           const data = {
             time: time,
             destination: destination,
-            code: item.MonitoredVehicleJourney.JourneyNote[0].value,
+            code: item.MonitoredVehicleJourney.JourneyNote[0]?.value,
           };
           results.push(data);
           return data;
         }
       );
-      const sortedResults = results.sort((a, b) => new Date(a.time) - new Date(b.time));
-      res.set('Content-Type', 'text/html');
+      const sortedResults = results.sort(
+        (a, b) => new Date(a.time) - new Date(b.time)
+      );
+      res.set("Content-Type", "text/html");
 
-      let content = '';
-      sortedResults.forEach(el => content += '<strong style="font-size: 1.25rem;">' + el.time + '</strong> - <em>' + el.code + '</em> - ' + el.destination + '<br><br>');
+      let content = "";
+      sortedResults.forEach(
+        (el) =>
+          (content +=
+            '<strong style="font-size: 1.25rem;">' +
+            el.time +
+            "</strong> - <em>" +
+            el.code +
+            "</em> - " +
+            el.destination +
+            "<br><br>")
+      );
 
-      res.send(Buffer.from(`<div style="font-family: Roboto, sans-serif;"><h3>Châtelet vers CDG/Mitry</h3>${content}</div>`));
+      res.send(
+        Buffer.from(
+          `<div style="font-family: Roboto, sans-serif;"><h3>Châtelet vers CDG/Mitry</h3>${content}</div>`
+        )
+      );
       // res.send(results.sort((a, b) => a.time - b.time));
-      console.table(results)
+      console.table(results);
       return results.sort((a, b) => a.time - b.time);
     })
     .catch((err) => console.error("error:" + err));
@@ -71,10 +106,20 @@ function getCDGRerB(res) {
       json.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit.forEach(
         (item) => {
           if (
-            item.MonitoredVehicleJourney.LineRef.value !== "STIF:Line::C01743:"
-          ) return;
-          if (item.MonitoredVehicleJourney.DestinationName[0].value.includes('CDG')) return;
-          if (item.MonitoredVehicleJourney.MonitoredCall.DepartureStatus === "cancelled") return;
+            item.MonitoredVehicleJourney.LineRef?.value !== "STIF:Line::C01743:"
+          )
+            return;
+          if (
+            item.MonitoredVehicleJourney.DestinationName[0]?.value.includes(
+              "CDG"
+            )
+          )
+            return;
+          if (
+            item.MonitoredVehicleJourney.MonitoredCall.DepartureStatus ===
+            "cancelled"
+          )
+            return;
 
           let time = new Date(
             item.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime
@@ -82,32 +127,68 @@ function getCDGRerB(res) {
 
           if (new Date() > time) return;
 
-          const timeFormatter = Intl.DateTimeFormat('fr-FR', { timeZone: "Europe/Paris", timeStyle: 'medium' });
+          const timeFormatter = Intl.DateTimeFormat("fr-FR", {
+            timeZone: "Europe/Paris",
+            timeStyle: "medium",
+          });
           time = timeFormatter.format(time);
 
-          const destination = item.MonitoredVehicleJourney.MonitoredCall.DestinationDisplay[0]
-            .value.slice(0, 22) + '...';
+          let destination =
+            item.MonitoredVehicleJourney.MonitoredCall.DestinationDisplay[0]
+              ?.value;
+
+              console.log(destination)
+
+          if (destination) {
+            destination = destination.slice(0, 22) + "...";
+          } else {
+            return;
+          }
+
+          if (destination.includes('Aéroport')) {
+            return;
+          }
 
           const data = {
             time: time,
             destination: destination,
-            code: item.MonitoredVehicleJourney.JourneyNote[0].value,
-            platform: item.MonitoredVehicleJourney.MonitoredCall.ArrivalPlatformName.value
+            code: item.MonitoredVehicleJourney.JourneyNote[0]?.value,
+            platform:
+              item.MonitoredVehicleJourney.MonitoredCall.ArrivalPlatformName
+                ?.value || '??',
           };
           results.push(data);
           return data;
         }
       );
-      const sortedResults = results.sort((a, b) => new Date(a.time) - new Date(b.time));
-      sortedResults.length = 5;
-      res.set('Content-Type', 'text/html');
+      const sortedResults = results.sort(
+        (a, b) => new Date(a.time) - new Date(b.time)
+      );
+      sortedResults.length = 8;
+      res.set("Content-Type", "text/html");
 
-      let content = '';
-      sortedResults.forEach(el => content += '<strong style="font-size: 1.25rem;">' + el.time + '</strong> -<em>' + el.platform + '- ' + el.code + '</em> ' + el.destination + '<br><br>');
+      let content = "";
+      sortedResults.forEach(
+        (el) =>
+          (content +=
+            '<strong style="font-size: 1.25rem;">' +
+            el.time +
+            "</strong> -<em>" +
+            el.platform +
+            "- " +
+            el.code +
+            "</em> " +
+            el.destination +
+            "<br><br>")
+      );
 
-      res.send(Buffer.from(`<div style="font-family: Roboto, sans-serif;"><h3>CDG Roissypôle vers le sud</h3>${content}</div>`));
+      res.send(
+        Buffer.from(
+          `<div style="font-family: Roboto, sans-serif;"><h3>CDG Roissypôle vers le sud</h3>${content}</div>`
+        )
+      );
       // res.send(results.sort((a, b) => a.time - b.time));
-      console.table(results)
+      console.table(results);
       return results.sort((a, b) => a.time - b.time);
     })
     .catch((err) => console.error("error:" + err));
